ccc2fd1dab2c1515ce09ff6112e079cf2c07fb3a
Demostat101 <demostat1995@gmail.com>
2025-01-26 05:33:17 +0100

    add

--- index.js
+++ index.js
@@ -9,8 +9,31 @@ const PORT = process.env.PORT || 3500;
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://hr360dashboard-omikunle-ademola.netlify.app',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, Postman)
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: ['GET', 'POST', 'OPTIONS',"PATCH","DELETE,PUT"], // Allowed methods
+  allowedHeaders: ['Content-Type'], // Allowed headers
+  credentials: true, // If you need to include credentials
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
+
+// Handle OPTIONS method for preflight requests
+app.options('*', cors(corsOptions)); // Enable preflight across all routes
+
 app.use(bodyParser.json());
-app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 app.use("/", routes);
 app.use(express.urlencoded({ extended: false }));
 
